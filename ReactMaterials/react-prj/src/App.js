4dfb1951ed6ab39d83be3cd1b01cc8388d5abb3c
yangMinjae <ymj137955@naver.com>
2025-06-04 09:29:43 +0900

    Created react-prj

---
+++ ReactMaterials/react-prj/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TestComponent from './components/TestComponent'
+function App() {
+  return (
+    <div className="App">
+      <TestComponent name='kim'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactMaterials/react-prj/src/components/TestComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestComponent = (props) => {
+  console.log(props);
+  return (
+    <div>
+      안녕하세요.
+    </div>
+  );
+};
+
+export default TestComponent;
